8656c04568fbd87942e3c9208bcb8fa9caa00a87
lynsunwoo <lynsunwoo@gmail.com>
2026-01-23 14:39:46 +0900

    메인페이지 수정 반응형 디자인

--- frontend/src/pages/HeroSection.js
+++ frontend/src/pages/HeroSection.js
@@ -3,23 +3,23 @@ import React from 'react';
 function HeroSection(props) {
   return (
     <article className="main_hero-section container">
-      <div className="grid ">
+      <div className="grid">
         <div className="col-8">
           <div className="top-text">
-            <h2>핀으로 더 정확해진 디자인 피드백</h2>
+            <h2>핀으로 더 정확해진 <span class="mo-br"></span>디자인 피드백</h2>
             <p>문제 되는 지점을 핀으로 찍고</p>
-            <p>위치기반 피드백으로 더 빠르게 개선하세요.</p>
+            <p>위치기반 피드백으로 <span class="mo-br"></span>더 빠르게 개선하세요.</p>
           </div>
-          <div className="hero-btns">
-            <button className='achive-btn'>아카이브 시작하기</button>
-            <button className='sample-btn'>샘플보기</button>
-          </div>
+          <img
+            className="hero-image "
+            src={process.env.PUBLIC_URL + '/images/hero_image.png'}
+            alt=""
+          />
+        </div><div className="hero-btns col-4">
+          <button className='achive-btn'>아카이브 시작하기</button>
+          <button className='sample-btn'>샘플보기</button>
         </div>
-        <img
-          className="hero-image col-4"
-          src={process.env.PUBLIC_URL + '/images/hero_image.png'}
-          alt=""
-        />
+
       </div>
 
     </article>
